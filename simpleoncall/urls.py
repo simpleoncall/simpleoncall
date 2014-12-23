716540e15a448084c86b6c78554f1024118a60b2
brettlangdon <brett@blangdon.com>
2014-12-23 15:02:12 -0500

    setup login form, add extra pages and account gravatar logo

--- simpleoncall/urls.py
+++ simpleoncall/urls.py
@@ -5,7 +5,11 @@ from simpleoncall import api
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'simpleoncall.views.index', name='index'),
+    url(r'^$', 'simpleoncall.views.dashboard', name='dashboard'),
+    url(r'^login', 'simpleoncall.views.login', name='login'),
+    url(r'^logout', 'simpleoncall.views.logout', name='logout'),
+    url(r'^settings', 'simpleoncall.views.settings', name='settings'),
+    url(r'^account', 'simpleoncall.views.account', name='account'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urlpatterns, namespace='api')),
 )
--- simpleoncall/views.py
+++ simpleoncall/views.py
@@ -1,5 +1,61 @@
+from functools import wraps
+
+from django.contrib.auth import logout as logout_user
+from django.contrib.auth import login as login_user, authenticate
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from simpleoncall.forms.auth import AuthenticationForm
 
-def index(request):
-    return render(request, 'index.html', {'text': 'hello world'})
+
+def require_authentication():
+    def wrapped(func):
+        @wraps(func)
+        def _wrapped(request, *args, **kwargs):
+            if not request.user.is_authenticated():
+                return HttpResponseRedirect(reverse('login'))
+            return func(request, *args, **kwargs)
+        return _wrapped
+    return wrapped
+
+
+@require_authentication()
+def dashboard(request):
+    return render(request, 'index.html', {'text': 'dashboard'})
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('dashboard'))
+
+    login_form = AuthenticationForm(request.POST or None)
+    if login_form.is_valid():
+        user_cache = authenticate(
+            username=login_form.cleaned_data['username'],
+            password=login_form.cleaned_data['password']
+        )
+        if user_cache:
+            login_user(request, user_cache)
+            return HttpResponseRedirect(reverse('dashboard'))
+
+    context = {
+        'form': login_form,
+    }
+    return render(request, 'auth/login.html', context)
+
+
+def logout(request):
+    logout_user(request)
+    return HttpResponseRedirect(reverse('login'))
+
+
+@require_authentication()
+def settings(request):
+    return render(request, 'index.html', {'text': 'settings'})
+
+
+@require_authentication()
+def account(request):
+    return render(request, 'index.html', {'text': 'account'})
