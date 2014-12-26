303b1fa2849c21441fb5e49faf6ca42e179b4db3
brettlangdon <brett@blangdon.com>
2014-12-26 18:09:01 -0500

    fix api url name

--- simpleoncall/api/__init__.py
+++ simpleoncall/api/__init__.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'simpleoncall.api.views.index', name='index'),
+    url(r'^$', 'simpleoncall.api.views.index', name='api-index'),
 )
