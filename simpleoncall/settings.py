e33200718b1ea3fc0cee13f99fac69705bf78e45
brettlangdon <brett@blangdon.com>
2015-01-18 19:25:39 -0500

    add settings to enable/disable SMS/Voice notifications

--- simpleoncall/settings.py
+++ simpleoncall/settings.py
@@ -101,6 +101,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+ALLOW_SMS = False
+ALLOW_VOICE = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
--- simpleoncall/tasks/notificatons.py
+++ simpleoncall/tasks/notificatons.py
@@ -1,7 +1,12 @@
+import logging
+
 from celery import shared_task
+from django.conf import settings
 
 from simpleoncall.models import Alert, User
 from simpleoncall.mail import send_alert_mail
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -13,9 +18,13 @@ def send_email_notification(user_id, alert_id):
 
 @shared_task
 def send_sms_notification(user_id, alert_id):
-    pass
+    if not settings.ALLOW_SMS:
+        logger.warning('SMS Notifications are disabled')
+        return
 
 
 @shared_task
 def send_voice_notification(user_id, alert_id):
-    pass
+    if not settings.ALLOW_VOICE:
+        logger.warning('Voice Notifications are disabled')
+        return
