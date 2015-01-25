ffc247045c7ce357871899c84fdfc61f4add06a9
brettlangdon <brett@blangdon.com>
2015-01-25 15:53:33 -0500

    add prototype of datadog integration endpoint closes 39

--- simpleoncall/api/v1/urls.py
+++ simpleoncall/api/v1/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     url(r'^alert/add$', 'simpleoncall.api.v1.views.alert_create', name='alert-add'),
     url(r'^alert/update$', 'simpleoncall.api.v1.views.alert_update', name='alert-update'),
     url(r'^alerts$', 'simpleoncall.api.v1.views.alerts_list', name='alerts-list'),
+    url(r'^integration/datadog$', 'simpleoncall.api.v1.views.datadog_integration', name='integration-datadog'),
 )
--- simpleoncall/api/v1/views.py
+++ simpleoncall/api/v1/views.py
@@ -1,6 +1,7 @@
 import datetime
+import json
 
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import IntegrityError
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
@@ -8,7 +9,7 @@ from django.utils import timezone
 
 from simpleoncall.decorators import requires_api_key, parse_body
 from simpleoncall.api import APIResponse
-from simpleoncall.models import Alert, EventStatus
+from simpleoncall.models import Alert, EventStatus, ServiceData, Services
 from simpleoncall.tasks.notificatons import send_alert_notifications
 
 
@@ -117,3 +118,76 @@ def alerts_list(request):
     results = [a.to_dict() for a in alerts]
 
     return APIResponse(result=results, status_code=200)
+
+
+@requires_api_key()
+@csrf_exempt
+@parse_body()
+def datadog_integration(request):
+    data = request.data or {}
+
+    # dont pay attention to any event type other than 'metric_alert_monitor'
+    if data.get('event_type') != 'metric_alert_monitor':
+        return APIResponse(result={})
+
+    aggregate_key = data.get('aggreg_key')
+
+    if not aggregate_key:
+        return APIResponse(error='Message must contain "aggreg_key"', status_code=400)
+
+    # try to look up existing service data for this specific event
+    service_data = None
+    try:
+        service_data = ServiceData.objects.get(service=Services.DATADOG, service_id=aggregate_key)
+    except ObjectDoesNotExist:
+        pass
+
+    # if we have service data, then we should have an alert attached to it, try to find it
+    alert = None
+    if service_data is not None:
+        try:
+            alert = Alert.objects.get(service_data=service_data, team=request.api_key.team)
+        except ObjectDoesNotExist:
+            pass
+    else:
+        service_data = ServiceData(service=Services.DATADOG, service_id=aggregate_key)
+
+    # an existing alert was not found, create a new one
+    if alert is None:
+        alert = Alert(service_data=service_data, team=request.api_key.team, created_by_api_key=request.api_key)
+
+    title = data.get('event_title')
+    body = data.get('event_msg')
+    metric = data.get('event_metric')
+    transition = data.get('alert_transition')
+
+    if not title:
+        title = 'Datadog event'
+        if metric:
+            title += ' for metric %s' % (metric, )
+        if transition:
+            title += ' %s' % (transition, )
+
+    alert.title = title
+    alert.body = body
+
+    if transition == 'Triggered':
+        alert.status = EventStatus.OPEN
+    elif transition == 'Recovered':
+        alert.status = EventStatus.RESOLVED
+
+    service_data.data = json.dumps(data)
+    try:
+        alert.save(api_key=request.api_key)
+        service_data.save(api_key=request.api_key)
+    except IntegrityError:
+        return APIResponse(error='Error while saving alert', status_code=400)
+
+    # TODO: should probably send a notification for other statuses too
+    if alert.status == EventStatus.OPEN:
+        schedule = request.api_key.team.get_active_schedule()
+        if schedule:
+            oncall = schedule.get_currently_on_call()
+            send_alert_notifications(oncall, alert)
+
+    return APIResponse(result={'id': alert.id}, status_code=201)
